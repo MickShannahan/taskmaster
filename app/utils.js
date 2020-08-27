01e8a7f2b269f5f495b31e5e5c7307a5b2543e9d
Mark R Ohnsman <MarkOhnsman@users.noreply.github.com>
2020-08-27 16:59:07 -0600

    Update utils.js

--- app/utils.js
+++ app/utils.js
@@ -5,9 +5,8 @@ export const generateId = function() {
   var result = "";
   var characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
   for (var i = 0; i < 10; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
 };
